9b3529e06466ff38be0816469fce0dce82ad90a4
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-09-06 19:17:57 -0300

    feat: add delete route to car router

--- src/routes/carRoute.ts
+++ src/routes/carRoute.ts
@@ -13,5 +13,6 @@ carRouter.post('/cars', (req, res) => carController.create(req, res));
 carRouter.get('/cars', (req, res) => carController.read(req, res));
 carRouter.get('/cars/:id', (req, res) => carController.readOne(req, res));
 carRouter.put('/cars/:id', (req, res) => carController.update(req, res));
+carRouter.delete('/cars/:id', (req, res) => carController.delete(req, res));
 
 export default carRouter;
