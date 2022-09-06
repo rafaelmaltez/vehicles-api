7809361990a4297687856386e5e3306b2cb2fae4
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-09-06 19:16:03 -0300

    feat: add delete method to Controller

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -29,4 +29,12 @@ export default abstract class Controller<T> {
     const result = await this.service.update(req.params.id, req.body);
     return res.status(200).json(result);
   }
+
+  public async delete(
+    req: Request & { params: { id: string } },
+    res: Response,
+  ) {
+    await this.service.delete(req.params.id);
+    return res.sendStatus(204);
+  }
 }
