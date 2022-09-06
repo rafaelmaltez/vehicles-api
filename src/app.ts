aef7a922b43ffe4c6e379f81b14128eec76e1ee1
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-09-06 16:03:52 -0300

    feat: add express-async-errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import carRoute from './routes/carRoute';
+import errorHandler from './middlewares/error';
 
 const app = express();
 app.use(express.json());
 app.use(carRoute);
+app.use(errorHandler);
 
 export default app;
