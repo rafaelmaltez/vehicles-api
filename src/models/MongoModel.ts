ca784133a1a8ff1fd83e1bf49af2627fa51e98fa
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-09-06 17:45:20 -0300

    feat: add update method to MongoModel

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -7,18 +7,25 @@ abstract class MongoModel<T> implements IModel<T> {
   constructor(model: Model<T>) {
     this._model = model;
   }
-
+  
   public async create(obj: T): Promise<T> {
     return this._model.create({ ...obj });
   }
-
+  
   public async read(): Promise<T[]> {
     return this._model.find();
   }
-
+  
   public async readOne(_id: string): Promise<T | null> {
     if (!isValidObjectId(_id)) throw new Error('InvalidMongoId');
     const result = await this._model.findOne({ _id });
+    if (!result) throw new Error('EntityNotFound');
+    return result;
+  }
+
+  public async update(_id: string, obj: T): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error('InvalidMongoId');
+    const result = await this._model.findOneAndUpdate({ _id });
     if (!result) throw new Error('EntityNotFound');
     return result;
   }
