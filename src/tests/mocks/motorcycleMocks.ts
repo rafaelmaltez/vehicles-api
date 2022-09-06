981f1c92b1acc212ae26bea7ae64b0fa2d5339cc
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-09-06 20:16:18 -0300

    test: add CarService tests

---
+++ src/tests/mocks/motorcycleMocks.ts
@@ -0,0 +1,18 @@
+export const motorcycleMock = {
+  model: 'Super Moto',
+  year: 2022,
+  color: 'Black',
+  buyValue: 19000,
+  category: 'Trail',
+  engineCapacity: 2499,
+}
+
+export const motorcycleMockWithId = {
+  _id: '62cf1fc6498565d94eba52cd',
+  model: 'Super Moto',
+  year: 2022,
+  color: 'Black',
+  buyValue: 19000,
+  category: 'Trail',
+  engineCapacity: 2499,
+}
--- src/tests/unit/services/car-service.test.ts
+++ src/tests/unit/services/car-service.test.ts
@@ -1,11 +1,15 @@
 import * as sinon from 'sinon';
 import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 const { expect } = chai;
+import { ZodError } from 'zod';
 import CarService from '../../../services/CarService';
 import CarModel from '../../../models/CarModel';
 import { carZodSchema } from '../../../interfaces/ICar';
 import { carMock, carMockWithId } from '../../mocks/carMocks';
 import { SafeParseReturnType } from 'zod';
+
+chai.use(chaiAsPromised)
 
 const makeSut = () => {
   const carModel = new CarModel()
@@ -22,6 +26,45 @@ describe('CarService', () => {
     sinon.stub(carModel, 'create').resolves(carMockWithId)
     sinon.stub(carZodSchema, 'safeParse').returns({ success: true, data: carMock})
     const result = await carService.create(carMock)
+    expect(result).to.be.deep.equal(carMockWithId)
+  })
+  it('dispara um erro caso valores invalidos sejam passados', async () => {
+    const { carService } = makeSut()
+    sinon.stub(carZodSchema, 'safeParse').returns({ success: false, error: new ZodError<any>([])})
+    const promise = carService.create(carMock)
+    return expect(promise).to.eventually.be.rejected
+  })
+
+  it('lista todos os carros com sucesso', async () => {
+    const { carService, carModel } = makeSut()
+    sinon.stub(carModel, 'read').resolves([carMockWithId, carMockWithId])
+    const result = await carService.read()
+    expect(result.length).to.be.equal(2)
+  })
+
+  it('lista um carro especifico com sucesso', async () => {
+    const { carService, carModel } = makeSut()
+    sinon.stub(carModel, 'readOne').resolves(carMockWithId)
+    const result = await carService.readOne('62cf1fc6498565d94eba52cd')
+    expect(result).to.be.deep.equal(carMockWithId)
+  })
+  it('atualiza um carro especifico com sucesso', async () => {
+    const { carService, carModel } = makeSut()
+    sinon.stub(carModel, 'update').resolves(carMockWithId)
+    const result = await carService.update('62cf1fc6498565d94eba52cd', carMock)
+    expect(result).to.be.deep.equal(carMockWithId)
+  })
+
+  it('dispara um erro ao atualizar caso valores invalidos sejam passados', async () => {
+    const { carService } = makeSut()
+    sinon.stub(carZodSchema, 'safeParse').returns({ success: false, error: new ZodError<any>([])})
+    const promise = carService.update('62cf1fc6498565d94eba52cd', carMock)
+    return expect(promise).to.eventually.be.rejected
+  })
+  it('deleta um carro especifico com sucesso', async () => {
+    const { carService, carModel } = makeSut()
+    sinon.stub(carModel, 'delete').resolves(carMockWithId)
+    const result = await carService.delete('62cf1fc6498565d94eba52cd')
     expect(result).to.be.deep.equal(carMockWithId)
   })
 });
