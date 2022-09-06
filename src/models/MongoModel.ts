1da5d014fd4f69316ddd9eb224d35f568d7676a8
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-09-06 15:12:40 -0300

    feat: create abstract MongoModel

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,29 @@
+import { isValidObjectId, Model } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  private _model: Model<T>;
+
+  constructor(model: Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async read(): Promise<T[]> {
+    return this._model.find();
+  }
+
+  public async readOne(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error('InvalidMongoId');
+    return this._model.findOne({ _id });
+  }
+
+  public async delete(_id: string): Promise<T | null> {
+    return this._model.findOneAndDelete({ _id });
+  }
+}
+
+export default MongoModel;
