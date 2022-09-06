87b5b3760e3094730bffca69e5ee3c2d50f781ef
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-09-06 15:45:39 -0300

    feat: create car router

---
+++ src/routes/carRoute.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import CarController from '../controllers/CarController';
+import CarService from '../services/CarService';
+import CarModel from '../models/CarModel';
+import { carZodSchema } from '../interfaces/ICar';
+
+const carModel = new CarModel();
+const carService = new CarService(carModel, carZodSchema);
+const carController = new CarController(carService);
+
+const carRouter = Router();
+carRouter.post('/cars', (req, res) => carController.create(req, res));
